497066c79162cb388d2e805d9ec88ad270c258f5
MarcusRei <marcusreineck@gmail.com>
2022-10-25 22:13:10 +0200

    debuggade js och fixade hamburger-symbol

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
-const hamburger = document.getElementsByClassName("hamburger");
-const navMenu = document.getElementsByClassName("nav-menu");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+console.log(hamburger);
 
 hamburger.addEventListener("click", expand);
 
 function expand() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  console.log("active");
 }
