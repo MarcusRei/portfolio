8b0df49b400b30a60cc1e851326787f001c9f88d
MarcusRei <marcusreineck@gmail.com>
2022-10-26 19:47:58 +0200

    fixade hamburger-meny, la till dynamik kring projekt

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,26 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
-console.log(hamburger);
+const hamburgerLines = document.querySelector(".hamburger-symbol");
+const navItems = document.querySelectorAll(".nav-item");
 
 hamburger.addEventListener("click", expand);
 
 function expand() {
-  hamburger.classList.toggle("active");
+  /* hamburger.classList.toggle("active"); */
   navMenu.classList.toggle("active");
-  console.log("active");
+  hamburgerLines.classList.toggle("active");
+
+  //QuerySelectorAll lägger alla element i en lista
+  for (let i = 0; i < 3; i++) {
+    navItems[i].classList.toggle("active");
+  }
+}
+
+const projectItem = document.querySelector(".project-item");
+console.log(projectItem);
+
+projectItem.addEventListener("click", enterProject);
+
+function enterProject() {
+  alert("Nu är du inne i det här projektet!");
 }
